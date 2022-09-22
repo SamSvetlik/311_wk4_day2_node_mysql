7dd0a9bc9839568c515a659368ee719091008b94
Daniel Vizueta <vizuetadaniel85@gmail.com>
2022-09-22 12:38:51 -0500

    done

--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,24 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating connection...')
+      console.log("creating connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+        host: "database-1.cg5jx2izu82w.us-east-2.rds.amazonaws.com",
+        user: "dvizueta",
+        password: "Ranger1985!",
+        database: "test",
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
